7ad32445345426f27d9b49089279147e22a8df1f
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2023-11-23 23:18:55 -0300

    Update load.js

--- eacnst/quiz/js/load.js
+++ eacnst/quiz/js/load.js
@@ -1,7 +1,12 @@
 function load(){
+    window.addEventListener('beforeunload', function(event) {
+      
+       history.replaceState("index.html")
+       console.log("ola")
+      });
+      
     history.pushState(null,null, "index.html")
     window.addEventListener('popstate', function () {
     history.pushState(null, null, "index.html")
-    location.href = 'index.html'
     // Adiciona um listener para o evento popstate
 })}
