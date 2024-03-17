25eb5be5d217ff0657ebed3d905ba29923e54e0c
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2024-03-17 18:22:13 -0300

    atualizei algumas coisas

--- interfaces-web2/responsividade/js/main.js
+++ interfaces-web2/responsividade/js/main.js
@@ -1,5 +1,5 @@
 
-// let width  = window.innerWidth
+let width  = window.innerWidth
 // var imagem = document.getElementsByClassName('imagem_carrossel')
 // if(width < 900){
 //     imagem[0].setAttribute('src', 'img/Banner_1_900-450.png')
@@ -48,3 +48,17 @@ const nav        = document.querySelector("#hamburguer")
 
 hamburguer.addEventListener("click", ()=>
 nav.classList.toggle("active"))
+
+if(width > 780){
+    function unwrap(element) {
+        const fragment = document.createDocumentFragment();
+        while (element.firstChild) {
+            const child = element.removeChild(element.firstChild);
+            fragment.appendChild(child);
+        }
+        element.parentNode.replaceChild(fragment, element);
+    }
+    
+    // Uso:
+    unwrap(nav);
+}
