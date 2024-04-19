6d0aae7f64a85e99753ac4bba1360a85bcb11165
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2024-04-19 20:26:47 -0300

    criei um tema escuro de um site

---
+++ interfaces-web2/javascript/script.js
@@ -0,0 +1,13 @@
+var botao      = document.querySelector(".material-symbols-outlined")
+var html       = document.querySelector("html")
+function trocarTema(){
+    if(botao.innerHTML == "dark_mode"){
+        html.style.setProperty("--cor01", "white")
+        html.style.setProperty("--cor02", "black")
+        botao.innerHTML = "brightness_5"
+    } else if(botao.innerHTML == "brightness_5"){
+        html.style.setProperty("--cor01", "black")
+        html.style.setProperty("--cor02", "white")
+        botao.innerHTML = "dark_mode"
+    }
+}
